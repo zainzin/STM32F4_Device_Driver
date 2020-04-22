5b7c9fad05498b8056e226ddd5d1296690c4022e
Zain Hatim <zain.hatim@gmail.com>
2020-04-21 23:47:10 -0400

    Initial

---
+++ Drivers/device_driver.h
@@ -0,0 +1,17 @@
+/*
+ * device_driver.h
+ *
+ *  Created on: Apr 21, 2020
+ *      Author: zainhatim
+ */
+
+#ifndef DEVICE_DRIVER_H_
+#define DEVICE_DRIVER_H_
+
+
+#define PERIPH_BASE 0x40000000U
+#define AHB1PERIPH_BASE (PERIPH_BASE + 0x20000U)
+#define RCC_BASE (AHB1PERIPH_BASE + 0x3800U)
+
+
+#endif /* DEVICE_DRIVER_H_ */
